14db953ed5b9feb2d2c0721ca61c27a0d352554b
Andre <aayandre@github.com>
2017-11-03 12:59:42 -0200

    só coisa boba

--- src/ProjetoIntegrado2/src/Mock/MockListaDeVenda.java
+++ src/ProjetoIntegrado2/src/Mock/MockListaDeVenda.java
@@ -16,61 +16,63 @@ import java.util.List;
  * @author aayan
  */
 public class MockListaDeVenda {
-    
+
     //Atributos
     private static ArrayList<Venda> vendas;
-    
+
     //Construtor
     public MockListaDeVenda() {
     }
-    
-    public List<Venda> getVendaByCod(int codVenda) {
-        
+
+    public List<Venda> getVendasByCod(int codVenda) {
+
         List<Venda> resultado = new ArrayList<>();
-        
-        if(codVenda > 0) {
+
+        if (codVenda > 0) {
             for (Venda venda : vendas) {
-                if(venda != null && venda.getCodVenda() > 0) {
-                   if(venda.getCodVenda() == codVenda) {
-                       resultado.add(venda);
-                   }
-                }
-            }
-        }
-        
-        return resultado;
-    }
-    
-    public List<Venda> getVendaByVendedor(String vendedor) {
-        
-        List<Venda> resultado = new ArrayList<>();
-        
-        if(vendedor != null) {
-            for (Venda venda : vendas) {
-                if(venda != null && venda.getVendedor() != null) {
-                    if(vendedor.toUpperCase().equals(venda.getVendedor().toUpperCase())) {
+                if (venda != null && venda.getCodVenda() > 0) {
+                    if (venda.getCodVenda() == codVenda) {
                         resultado.add(venda);
                     }
                 }
             }
         }
-        
+
         return resultado;
     }
-    
-    public List<Venda> getVendaByData(Calendar de, Calendar ate) {
-        
+
+    public List<Venda> getVendasByVendedor(String vendedor) {
+
+        List<Venda> resultado = new ArrayList<>();
+
+        if (vendedor != null) {
+            for (Venda venda : vendas) {
+                if (venda != null && venda.getVendedor() != null) {
+                    if (vendedor.toUpperCase().equals(venda.getVendedor().toUpperCase())) {
+                        resultado.add(venda);
+                    }
+                }
+            }
+        }
+
+        return resultado;
+    }
+
+    public List<Venda> getVendasByData(Calendar de, Calendar ate) {
+
         List<Venda> resultado = new ArrayList<>();
         Calendar agora = Calendar.getInstance();
-        
-        if(!de.after(agora) || !ate.after(agora)) {
-            
+
+        if (!de.after(agora) || !ate.after(agora)) {
+
         }
-        
+
         return resultado;
     }
-    
-    public List<Venda> getVendaByCliente(Cliente cliente) {
+
+    public List<Venda> getVendasByCliente(Cliente cliente) {
+        List<Venda> resultado = new ArrayList<>();
+
         return null;
     }
 }
